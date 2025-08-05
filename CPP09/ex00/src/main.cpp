708663fe8b4e94cb61aaf8b06bd5be490063ab5d
joklein <joklein@student.42heilbronn.de>
2025-08-05 16:39:23 +0200

    Reset the color after Error in the main

--- CPP09/ex00/src/main.cpp
+++ CPP09/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joklein <joklein@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 12:10:34 by joklein           #+#    #+#             */
-/*   Updated: 2025/07/24 10:38:36 by joklein          ###   ########.fr       */
+/*   Updated: 2025/08/05 16:38:57 by joklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int argc, char **argv)
 {
 	if(argc != 2)
-		return(std::cerr << RED << "Error: could not open file." << std::endl, 1);
+		return(std::cerr << RED << "Error: could not open file." << RESET << std::endl, 1);
 		
 	try{
 		std::ifstream input_file(argv[1]);
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 		btc.process_btc(input_file);
 		input_file.close();
 	} catch(const std::exception &e){
-		std::cerr << RED << e.what() << std::endl;
+		std::cerr << RED << e.what() << RESET << std::endl;
 		return(1);
 	}
 }
