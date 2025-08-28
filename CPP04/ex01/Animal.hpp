f0d62208297c54e4c13433449c4459552f8f65e0
joklein <joklein@student.42heilbronn.de>
2025-08-28 10:59:06 +0200

    change header format

--- CPP04/ex01/Animal.hpp
+++ CPP04/ex01/Animal.hpp
@@ -6,29 +6,29 @@
 /*   By: joklein <joklein@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:31:38 by joklein           #+#    #+#             */
-/*   Updated: 2025/05/26 11:41:48 by joklein          ###   ########.fr       */
+/*   Updated: 2025/08/28 10:55:17 by joklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ANIMAL_HPP
-# define ANIMAL_HPP
+#define ANIMAL_HPP
 
-# include <iostream>
+#include <iostream>
 
 class Animal
 {
   public:
-	  Animal();
-    Animal(const std::string& type);
-	  Animal(const Animal &other);
-	  Animal &operator=(const Animal &other);
-	  virtual ~Animal();
+	Animal();
+	Animal(const std::string &type);
+	Animal(const Animal &other);
+	Animal &operator=(const Animal &other);
+	virtual ~Animal();
 
-    virtual void makeSound() const;
-    std::string getType() const;
+	virtual void makeSound() const;
+	std::string getType() const;
 
   protected:
-	  std::string m_type;
+	std::string m_type;
 };
 
 #endif
