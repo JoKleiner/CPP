e228ca033079a0eb24686bbfc43a8eed887f31c3
joklein <joklein@student.42heilbronn.de>
2025-08-28 11:47:27 +0200

    change format

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,29 +6,27 @@
 /*   By: joklein <joklein@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 16:25:45 by joklein           #+#    #+#             */
-/*   Updated: 2025/07/07 14:47:08 by joklein          ###   ########.fr       */
+/*   Updated: 2025/08/28 11:46:58 by joklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-int main( void )
+int main(void)
 {
     int a = 2;
     int b = 3;
-    
-    ::swap( a, b );
+
+    swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-    
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+
     std::string c = "chaine1";
     std::string d = "chaine2";
-    
-    ::swap(c, d);
+
+    swap(c, d);
     std::cout << "\nc = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    
-    return 0;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 }
