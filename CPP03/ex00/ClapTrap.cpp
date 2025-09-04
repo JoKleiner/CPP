5ede552f9b400a1e9dc3cc84ec498485a1bf8e52
joklein <joklein@student.42heilbronn.de>
2025-09-04 10:39:25 +0200

    change some format

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: joklein <joklein@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 10:38:29 by joklein           #+#    #+#             */
-/*   Updated: 2025/09/04 10:21:24 by joklein          ###   ########.fr       */
+/*   Updated: 2025/09/04 10:35:40 by joklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,14 +48,12 @@ ClapTrap::~ClapTrap(){
 
 void ClapTrap::attack(const std::string &target)
 {
-	if (m_hit_points <= 0)	{
-		std::cout << "ClapTrap " << m_name << " is out of hit-points" << std::endl;
-		return ;
-	}
-	if (m_energy_points <= 0)	{
-		std::cout << "ClapTrap " << m_name << " is out of energy-points" << std::endl;
-		return ;
-	}
+	if (m_hit_points <= 0)
+		return (std::cout << "ClapTrap " << m_name << " is out of hit-points" << std::endl, void());
+		
+	if (m_energy_points <= 0)
+		return (std::cout << "ClapTrap " << m_name << " is out of energy-points" << std::endl, void());
+
 	std::cout << "ClapTrap " << m_name << " attacks " << target << ", causing " << m_attack_dmg << " points of damage!" << std::endl;
 	m_energy_points--;
 	std::cout << "ClapTrap " << m_name << " | hit-points: " << m_hit_points << ", energy-points: " << m_energy_points << std::endl;
@@ -69,14 +67,12 @@ void ClapTrap::takeDamage(unsigned int amount){
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	if (m_hit_points <= 0)	{
-		std::cout << "ClapTrap " << m_name << " is out of hit-points" << std::endl;
-		return ;
-	}
-	if (m_energy_points <= 0)	{
-		std::cout << "ClapTrap " << m_name << " is out of energy-points" << std::endl;
-		return ;
-	}
+	if (m_hit_points <= 0)
+		return (std::cout << "ClapTrap " << m_name << " is out of hit-points" << std::endl, void());
+	
+	if (m_energy_points <= 0)
+		std::cout << "ClapTrap " << m_name << " is out of energy-points" << std::endl, void());
+
 	std::cout << "ClapTrap " << m_name << " repaires " << amount << " Hit-points!" << std::endl;
 	m_hit_points += amount;
 	m_energy_points--;
--- CPP03/ex02/FragTrap.cpp
+++ CPP03/ex02/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: joklein <joklein@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:59:48 by joklein           #+#    #+#             */
-/*   Updated: 2025/05/21 13:32:09 by joklein          ###   ########.fr       */
+/*   Updated: 2025/09/04 10:38:47 by joklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,10 @@ FragTrap &FragTrap::operator=(const FragTrap &other)
 	std::cout << "Copy FragTrap assignment operator called" << std::endl;
 	if (this != &other)
 	{
-		this->m_name = other.m_name;
-		this->m_hit_points = other.m_hit_points;
-		this->m_energy_points = other.m_energy_points;
-		this->m_attack_dmg = other.m_attack_dmg;
+		m_name = other.m_name;
+		m_hit_points = other.m_hit_points;
+		m_energy_points = other.m_energy_points;
+		m_attack_dmg = other.m_attack_dmg;
 	}
 	return (*this);
 }
@@ -53,22 +53,17 @@ FragTrap::~FragTrap(){
 
 void FragTrap::attack(const std::string &target)
 {
-	if (this->m_hit_points <= 0)
-	{
-		std::cout << "FragTrap " << m_name << " is out of hit-points" << std::endl;
-		return ;
-	}
-	if (this->m_energy_points <= 0)
-	{
-		std::cout << "FragTrap " << m_name << " is out of energy-points" << std::endl;
-		return ;
-	}
+	if (m_hit_points <= 0)
+		return (std::cout << "FragTrap " << m_name << " is out of hit-points" << std::endl, void());
+		
+	if (m_energy_points <= 0)
+		return (std::cout << "FragTrap " << m_name << " is out of energy-points" << std::endl, void());
+		
 	std::cout << "FragTrap " << m_name << " attacks " << target << ", causing " << m_attack_dmg << " points of damage!" << std::endl;
-	this->m_energy_points--;
+	m_energy_points--;
 	std::cout << "FragTrap " << m_name << " | hit-points: " << m_hit_points << ", energy-points: " << m_energy_points << std::endl;
 }
 
-void FragTrap::highFivesGuys(void)
-{
+void FragTrap::highFivesGuys(void){
     std::cout << m_name << ": \033[1;31m\"wow: \033[0;3;33mhigh5..\033[1;31m\"\033[0m" << std::endl;
 }
