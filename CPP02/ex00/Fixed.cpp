94f71db78a3f18f7d592aa28a7189562ba87325b
joklein <joklein@student.42heilbronn.de>
2025-09-04 10:00:44 +0200

    change some foramt

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: joklein <joklein@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 12:56:00 by joklein           #+#    #+#             */
-/*   Updated: 2025/09/04 09:59:07 by joklein          ###   ########.fr       */
+/*   Updated: 2025/09/04 10:00:21 by joklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ Fixed &Fixed::operator=(const Fixed &other)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
-		this->m_fPValue = other.getRawBits();
+		m_fPValue = other.getRawBits();
 	return (*this);
 }
 
@@ -35,10 +35,10 @@ Fixed::~Fixed(){
 
 int Fixed::getRawBits(void) const{
 	std::cout << "getRawBits member function called" << std::endl;
-	return (this->m_fPValue);
+	return (m_fPValue);
 }
 
 void Fixed::setRawBits(int const raw){
 	std::cout << "setRawBits member function called" << std::endl;
-	this->m_fPValue = raw;
+	m_fPValue = raw;
 }
